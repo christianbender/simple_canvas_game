2f86cb87acc7cef26f6a292d080a32ff135b2935
Christian Bender <christianbender89@web.de>
2018-04-11 16:47:29 +0200

    hero can't leave the play field.

--- js/game.js
+++ js/game.js
@@ -65,16 +65,20 @@ var reset = function () {
 // Update game objects
 var update = function (modifier) {
 	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
+		if (hero.y >= 32)
+			hero.y -= hero.speed * modifier;
 	}
 	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
+		if (hero.y < HEIGHT-64)
+			hero.y += hero.speed * modifier;
 	}
 	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
+		if (hero.x > 32)
+			hero.x -= hero.speed * modifier;
 	}
 	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
+		if (hero.x < WIDTH-64)
+			hero.x += hero.speed * modifier;
 	}
 
 	// Are they touching?
